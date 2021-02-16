2eef3ef1f632622d4d62019ebc0159bc8ccd3aa3
Paul Mulliganiel <classicjazz@gmail.com>
2021-02-16 11:30:28 -0800

    Site back in working condition

--- header.php
+++ header.php
@@ -7,10 +7,10 @@
 
 	<head>
 		<meta charset="utf-8">
-		<?php // force Internet Explorer to use the latest rendering engine available ?>
+		<?php // force Internet Explorer to use the latest rendering engine available?>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<title><?php wp_title(''); ?></title>
-		<?php // mobile meta (hooray!) ?>
+		<?php // mobile meta (hooray!)?>
 		<meta name="HandheldFriendly" content="True">
 		<meta name="MobileOptimized" content="320">
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
@@ -22,8 +22,8 @@
 
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 		<?php wp_head(); ?>
-		<?php // drop Google Analytics Here ?>
-		<?php // end analytics ?>
+		<?php // drop Google Analytics Here?>
+		<?php // end analytics?>
 	</head>
 	<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
 		<div id="container" class="push">
@@ -34,18 +34,18 @@
 					<?php get_sidebar('sidebar2'); ?>
 					<nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement" id="primary-menu">
 						<?php wp_nav_menu(array(
-    					         'container' => false,                           // remove nav container
-    					         'container_class' => 'menu cf',                 // class of container (should you choose to use it)
-    					         'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
-    					         'menu_class' => 'nav top-nav cf',               // adding custom nav class
-    					         'theme_location' => 'main-nav',                 // where it's located in the theme
-    					         'before' => '',                                 // before the menu
-        			               'after' => '',                                  // after the menu
-        			               'link_before' => '',                            // before each link
-        			               'link_after' => '',                             // after each link
-        			               'depth' => 0,                                   // limit the depth of the nav
-    					         'fallback_cb' => ''                             // fallback function (if there is one)
-						)); ?>
+                                 'container' => false,                           // remove nav container
+                                 'container_class' => 'menu cf',                 // class of container (should you choose to use it)
+                                 'menu' => __('The Main Menu', 'bonestheme'),  // nav name
+                                 'menu_class' => 'nav top-nav cf',               // adding custom nav class
+                                 'theme_location' => 'main-nav',                 // where it's located in the theme
+                                 'before' => '',                                 // before the menu
+                                   'after' => '',                                  // after the menu
+                                   'link_before' => '',                            // before each link
+                                   'link_after' => '',                             // after each link
+                                   'depth' => 0,                                   // limit the depth of the nav
+                                 'fallback_cb' => ''                             // fallback function (if there is one)
+                        )); ?>
 					</nav>
 				</div>
 			</header>
