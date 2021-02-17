7b444270052c067adf8a42a86a43e92e640893a6
Paul Mulliganiel <classicjazz@gmail.com>
2021-02-17 11:17:18 -0800

    Remove whitespace

--- search.php
+++ search.php
@@ -1,20 +1,12 @@
 <?php get_header(); ?>
-
 			<div id="content">
-
 				<div id="inner-content" class="wrap cf">
-
 					<main id="main" class="m-all t-2of3 d-5of7 cf" role="main">
 						<h1 class="archive-title"><span><?php _e( 'Search Results for:', 'pulllisttheme' ); ?></span> <?php echo esc_attr(get_search_query()); ?></h1>
-
 						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
 							<article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article">
-
 								<header class="entry-header article-header">
-
 									<h3 class="search-title entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-
                   						<p class="byline entry-meta vcard">
                     							<?php printf( __( 'Posted %1$s by %2$s', 'pulllisttheme' ),
                    							    /* the time the post was published */
@@ -23,32 +15,20 @@
                        							    '<span class="by">by</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
                     							); ?>
                   						</p>
-
 								</header>
-
 								<section class="entry-content">
 										<?php the_excerpt( '<span class="read-more">' . __( 'Read more &raquo;', 'pulllisttheme' ) . '</span>' ); ?>
-
 								</section>
-
 								<footer class="article-footer">
-
 									<?php if(get_the_category_list(', ') != ''): ?>
                   					<?php printf( __( 'Filed under: %1$s', 'pulllisttheme' ), get_the_category_list(', ') ); ?>
                   					<?php endif; ?>
-
                  					<?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'pulllisttheme' ) . '</span> ', ', ', '</p>' ); ?>
-
 								</footer> <!-- end article footer -->
-
 							</article>
-
 						<?php endwhile; ?>
-
 								<?php pull_list_page_navi(); ?>
-
 							<?php else : ?>
-
 									<article id="post-not-found" class="hentry cf">
 										<header class="article-header">
 											<h1><?php _e( 'Sorry, No Results.', 'pulllisttheme' ); ?></h1>
@@ -60,15 +40,9 @@
 												<p><?php _e( 'This is the error message in the search.php template.', 'pulllisttheme' ); ?></p>
 										</footer>
 									</article>
-
 							<?php endif; ?>
-
 						</main>
-
 							<?php get_sidebar(); ?>
-
 					</div>
-
 			</div>
-
 <?php get_footer(); ?>
